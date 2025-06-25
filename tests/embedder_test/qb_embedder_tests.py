53e0362f104f0098e7a3a05681b985304799c77f
Simon <bosen.xu@mail.utoronto.ca>
2025-06-25 08:23:26 +0800

    Created test file for QBEmbedder

---
+++ tests/embedder_test/qb_embedder_tests.py
@@ -0,0 +1,8 @@
+import pytest
+import logging
+from logging import Logger
+
+from qb.question_bank import QuestionBank
+from qb.question import Question
+from embedder.qb_embedder import QBEmbedder
+
