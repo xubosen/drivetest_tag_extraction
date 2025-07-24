3f4c3d88c5b2644c4257ec4dca59dcc44742620f
Simon <bosen.xu@mail.utoronto.ca>
2025-07-25 02:08:22 +0800

    Added validation of urls with models.

--- src/label_generator/labeling_request.py
+++ src/label_generator/labeling_request.py
@@ -1,11 +1,35 @@
 import json
 from typing import List, Dict
-from pydantic import BaseModel, Field, field_validator
+from pydantic import BaseModel, Field, field_validator, model_validator
+
+URL_TO_MODELS = {
+    "/v1/chat/ds-test": {"batch-test-model"},
+    "/v1/chat/completions": {'deepseek-r1',
+                             'deepseek-v3',
+                             'qwen-long',
+                             'qwen-long-latest',
+                             'qwen-max',
+                             'qwen-max-latest',
+                             'qwen-plus',
+                             'qwen-plus-latest',
+                             'qwen-turbo',
+                             'qwen-turbo-latest',
+                             'qwq-32b-preview',
+                             'qwq-plus',
+                             'qwen-vl-max',
+                             'qwen-vl-max-latest',
+                             'qwen-vl-plus'
+                             'qwen-omni-turbo'},
+    "/v1/embeddings": {"text-embedding-v1, "
+                       "text-embedding-v2, "
+                       "text-embedding-v3, "
+                       "text-embedding-v4"}
+}
 
 class LabelingRequest(BaseModel):
     custom_id: str = Field(..., min_length=1)
     _method: str = "POST"
-    _url: str = "/v1/chat/completions"
+    url: str = Field(..., description="URL for the request")
     model: str = Field(..., min_length=1,
                        description="Model to use for the request")
     prompt: str = Field(..., min_length=1,
@@ -13,11 +37,18 @@ class LabelingRequest(BaseModel):
     content: List[Dict[str, str]] = Field(..., min_length=1,
                                           description="List of messages")
 
-    @field_validator(mode='after')
+    @field_validator("url", mode="after")
     @classmethod
-    def validate_content(cls, inputs: List[Dict[str, str]]) \
+    def validate_url(cls, url:str) -> str:
+        if url not in URL_TO_MODELS.keys():
+            raise ValueError(f"URL must be one of {URL_TO_MODELS.keys()}")
+        return url
+
+    @field_validator("content", mode='after')
+    @classmethod
+    def validate_content(cls, content: List[Dict[str, str]]) \
             -> List[Dict[str, str]]:
-        for input in inputs:
+        for input in content:
             if len(input) != 2:
                 raise ValueError("Each content item must have exactly two keys:"
                                  "'type' and the corresponding content key")
@@ -29,7 +60,16 @@ class LabelingRequest(BaseModel):
             if input["type"] not in input.keys():
                 raise ValueError(f"Content item of type '{input['type']}' "
                                  "must have a corresponding key")
-        return inputs
+        return content
+
+    @model_validator(mode='after')
+    def validate_model_match_url(self):
+        """ Check that the model matches the url. """
+        if self.model not in URL_TO_MODELS[self.url]:
+            raise ValueError(f"Model '{self.model}' is not valid for URL "
+                             f"'{self.url}'. Valid models are: "
+                             f"{URL_TO_MODELS[self.url]}")
+        return self
 
     def __str__(self) -> str:
         """
@@ -44,7 +84,7 @@ class LabelingRequest(BaseModel):
         json_dict = {
             "custom_id": self.custom_id,
             "method": self._method,
-            "url": self._url,
+            "url": self.url,
             "body": {
                 "model": self.model,
                 "messages": [
@@ -53,7 +93,7 @@ class LabelingRequest(BaseModel):
                 ]
             }
         }
-        return json.dumps(json_dict, ensure_ascii=False, indent=0)
+        return json.dumps(json_dict, ensure_ascii=False)
 
 class Config:
     validate_assignment = True
